685038cb747be28850fe7aa80ce58af00bcb07dd
Oslodo <r.tyler.swan@gmail.com>
2024-06-04 20:58:35 -0300

    More work

---
+++ FF1Lib/League/LeagueWIP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FF1Lib.GameClasses;
+
+namespace FF1Lib.League
+{
+	public partial class GameClasses
+	{
+
+		public class BonusMalusLeagueWIP
+		{
+			public List<Item> Equipment { get; set; }
+			public List<SpellSlots> SpellList { get; set; }
+			public List<bool> StatGrowth { get; set; }
+			public int StatMod { get; set; }
+			public int StatMod2 { get; set; }
+			public RankedType TargetStat { get; set; }
+			public BonusMalusAction Action { get; set; }
+			public string Description { get; set; }
+			public List<byte> SpcGrowth { get; set; }
+			public List<Classes> ClassList { get; set; }
+			public SpellSlotInfo SpellSlotMod { get; set; }
+			public List<SpellSlotInfo> SpellsMod { get; set; }
+			public BonusMalusLeagueWIP(BonusMalusAction action, string description, int mod = 0, int mod2 = 0, List<Item> equipment = null, List<bool> binarylist = null, List<SpellSlots> spelllist = null, List<byte> bytelist = null, SpellSlotInfo spellslotmod = null, List<SpellSlotInfo> spellsmod = null, List<Classes> Classes = null)
+			{
+				Action = action;
+				Description = description;
+				StatMod = mod;
+				StatMod2 = mod2;
+				Equipment = equipment;
+				SpellList = spelllist;
+				StatGrowth = binarylist;
+				SpellSlotMod = spellslotmod;
+				SpellsMod = spellsmod;
+				if (bytelist == null)
+					SpcGrowth = Enumerable.Repeat((byte)0x00, 49).ToList();
+				else
+					SpcGrowth = bytelist;
+				if (Classes == null)
+					ClassList = Enum.GetValues<Classes>().ToList();
+				else
+					ClassList = Classes;
+			}
+
+		}
+	}
+}
